b01cbf62e089ecb1f6b7b129801685f5d39a9c50
Fred Ross <fross@fross-vm-host.(none)>
2013-06-13 13:43:57 -0700

    Add comment on where tab group code came from.

--- com.splunk.dev.agent.launcher.ui/src/com/splunk/dev/agent/launcher/ui/MonitoredLaunchConfigurationTabGroup.java
+++ com.splunk.dev.agent.launcher.ui/src/com/splunk/dev/agent/launcher/ui/MonitoredLaunchConfigurationTabGroup.java
@@ -25,6 +25,15 @@ public class MonitoredLaunchConfigurationTabGroup extends
 	@Override
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
 		
+		// This code is copied from the method on JavaLaunchConfigurationTabGroup,
+		// with the sole exception of inserting "new SplunkTab()," right before
+		// "new CommonTab()". This set of tabs changes very rarely, so it is
+		// cleaner to copy this one statement than to try to dig into the
+		// JDT dynamically to get the tab list.
+		//
+		// If you are adding additional tabs, they should go after SplunkTab
+		// but before CommonTab. Eclipse's UI guidelines specify that CommonTab
+		// should always be last.
 		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
 				new JavaMainTab(),
 				new JavaArgumentsTab(),
